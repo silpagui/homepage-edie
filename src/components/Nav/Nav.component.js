a5aeb45cfa8f16588b308738e71ec9127382a59f
silpagui <silpagui.sa@gmail.com>
2021-09-02 18:34:36 +0200

    feat(components): create Nav component

---
+++ src/components/Nav/Nav.component.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export function Nav() {
+  return (
+    <ul>
+      <li>
+        <a href="#home">Home</a>
+      </li>
+      <li>
+        <a href="#services">Services</a>
+      </li>
+      <li>
+        <a href="#our-work">Our Work</a>
+      </li>
+      <li>
+        <a href="#clients">Clients</a>
+      </li>
+      <li>
+        <a href="#footer">Contact</a>
+      </li>
+    </ul>
+  );
+}
